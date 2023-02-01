caaa21e212849225d3d985d1bc72ecff599b5eb6
pouriashiralipour <pouriashiralipour@gmail.com>
2023-02-01 17:30:59 +0330

    create server and port for server

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+//  import library we need
 const express = require('express')
 const app = express()
 const http = require('http')
+
+// create server 
+const server = http.createServer(app)
+
+// create port for server
+const port = process.env.PORT || 3000
